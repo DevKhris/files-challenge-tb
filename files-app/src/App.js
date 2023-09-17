25d4fba80c7954c0c5cb1ad84f4b613dd34f7186
Christian H <contact@devkhris.com>
2023-09-16 22:45:53 -0600

    added loading messages

--- files-app/src/App.js
+++ files-app/src/App.js
@@ -1,20 +1,48 @@
 import Alert from "react-bootstrap/Alert";
-import { useEffect, useState, useMemo } from "react";
+import { useEffect, useState, useMemo, useRef, Suspense } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Table from "react-bootstrap/Table";
+import Form from "react-bootstrap/Form";
 
 function App() {
   const [files, setFiles] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const originalFiles = useRef();
+
   useEffect(() => {
+    setLoading(true);
     fetch(`http://localhost:8080/files/data`)
+      .then((response) => response.json())
+      .then((data) => {
+        originalFiles.current = data;
+        setFiles(data);
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+      .catch((err) => setError(err.message));
+  }, []);
+
+  const visibleFiles = useMemo(() => {
+    return files !== null ? files : originalFiles;
+  }, [files, originalFiles]);
+
+  const handleInputChange = (e) => {
+    const fileName = e.target.value;
+    setLoading(true);
+    fetch(`http://localhost:8080/files/data?fileName=${fileName}`)
       .then((response) => response.json())
       .then((data) => {
         setFiles(data);
       })
+      .finally(() => {
+        setLoading(false);
+      })
       .catch((err) => setError(err.message));
-  }, []);
+  };
 
   return (
     <div>
@@ -27,30 +55,41 @@ function App() {
       </Navbar>
       <main>
         <Container>
-          {error && <Alert className="mt-5 alert-danger">{error}</Alert>}
-          <Table responsive className="mt-5">
-            <thead>
-              <tr>
-                <th>File Name</th>
-                <th>Text</th>
-                <th>Number</th>
-                <th>Hex</th>
-              </tr>
-            </thead>
-            <tbody>
-              {files &&
-                files.map((file) => {
-                  return (
-                    <tr>
-                      <td>{file.file}</td>
-                      <td>{file.text}</td>
-                      <td>{file.number}</td>
-                      <td>{file.hex}</td>
-                    </tr>
-                  );
-                })}
-            </tbody>
-          </Table>
+          {error && <Alert className="my-5 alert-danger">{error}</Alert>}
+          <Form.Control
+            className="mt-5"
+            placeholder="Search for a file"
+            type="text"
+            aria-describedby="searchBlock"
+            onChange={handleInputChange}
+          />
+          {loading && <p className="my-3 text-center">Loading...</p>}
+          <Suspense>
+            <Table responsive className="my-5" striped>
+              <thead>
+                <tr>
+                  <th>File Name</th>
+                  <th>Text</th>
+                  <th>Number</th>
+                  <th>Hex</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {visibleFiles &&
+                  visibleFiles.map((file) => {
+                    return (
+                      <tr key={file.hex}>
+                        <td>{file.file}</td>
+                        <td>{file.text}</td>
+                        <td>{file.number}</td>
+                        <td>{file.hex}</td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+            </Table>
+          </Suspense>
         </Container>
       </main>
     </div>
