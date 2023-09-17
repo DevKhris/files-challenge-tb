0b076af8d1e453d00a07bac815ef76204f963dae
Christian H <contact@devkhris.com>
2023-09-16 22:17:09 -0600

    fixed typo in api and added cors and logger

--- files-api/index.js
+++ files-api/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 const app = express();
 
 const port = 8080;
@@ -7,7 +9,8 @@ const filesRoutes = require("./routes/files.routes");
 
 app.use(express.json({ type: "application/json", strict: true }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors());
+app.use(morgan("combined"));
 app.use("/files", filesRoutes);
 
-app.listen(port, () => console.log(`Running on http://localhost${port}`));
+app.listen(port, () => console.log(`Running on http://localhost:${port}`));
