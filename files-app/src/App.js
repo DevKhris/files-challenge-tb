f2399c24ccf7f2bc6844ab30d52c1db4092e338c
Christian H <contact@devkhris.com>
2023-09-16 21:57:44 -0600

    frontend base

--- files-app/src/App.js
+++ files-app/src/App.js
@@ -1,5 +1,51 @@
+import { useEffect, useState, useMemo } from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Table from "react-bootstrap/Table";
+
 function App() {
-  return <div className="container-fluid"></div>;
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {}, [files]);
+
+  return (
+    <div>
+      <Navbar color="" expand="lg" className="bg-danger">
+        <Container>
+          <Navbar.Brand className="text-white" href="#home">
+            React Test App
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <main>
+        <Container>
+          <Table responsive className="mt-5">
+            <thead>
+              <tr>
+                <th>File Name</th>
+                <th>Text</th>
+                <th>Number</th>
+                <th>Hex</th>
+              </tr>
+            </thead>
+            <tbody>
+              {files &&
+                files.map((file) => {
+                  return (
+                    <tr>
+                      <td>{file.file}</td>
+                      <td>{file.text}</td>
+                      <td>{file.number}</td>
+                      <td>{file.hex}</td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </Table>
+        </Container>
+      </main>
+    </div>
+  );
 }
 
 export default App;
