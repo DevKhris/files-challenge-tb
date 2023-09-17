1f1b6b4229f67c5684f92ff4e55112d84a43d8d8
Christian H <contact@devkhris.com>
2023-09-16 21:10:51 -0600

    updated tests

--- files-api/routes/files.routes.js
+++ files-api/routes/files.routes.js
@@ -1,4 +1,4 @@
-const { Router, json } = require("express");
+const { Router } = require("express");
 const router = Router();
 
 const apiURL = "https://echo-serv.tbxnet.com/v1/secret";
@@ -9,16 +9,16 @@ const getFormattedCsv = require("../utils/getFormattedCsv");
 router.get("/data", async (req, res) => {
   try {
     const results = [];
-
     const filesData = await getFiles(apiURL, token);
+    //  loop trough the file name list and get the formatted csv's then push to array
     for (fileName of filesData.files) {
       const result = await getFormattedCsv(apiURL, fileName, token);
       results.push(result);
     }
 
+    // search by query param with flatting and filtering the results array
     if (req.query.fileName) {
       let fileName = req.query.fileName;
-
       const filteredResult = results
         .flat()
         .filter((obj) => obj.file === fileName);
