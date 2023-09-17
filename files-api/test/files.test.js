dd0f6160e984beedf56caf161e72419e946abdee
Christian H <contact@devkhris.com>
2023-09-16 21:00:46 -0600

    updated test for query param

--- files-api/test/files.test.js
+++ files-api/test/files.test.js
@@ -9,10 +9,27 @@ const testApiURL = "http://localhost:8080/files";
 
 describe("GET /files", function () {
   describe("test formatting and listing endpoints with 200 code", function () {
-    it("should return a formatted response with the content of csv files from data endpoint", function (done) {
+    it("should return a response with the formatted content of the csv files from data endpoint", function (done) {
       chai
         .request(testApiURL)
         .get("/data")
+        .end(function (err, res) {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an("array").that.is.not.empty;
+          expect(res.body[0]).to.have.deep.keys([
+            "file",
+            "text",
+            "number",
+            "hex",
+          ]);
+          done(err);
+        });
+    });
+
+    it("should return a response with the formatted content of a file using the query params in data endpoint", function (done) {
+      chai
+        .request(testApiURL)
+        .get("/data?fileName=test3.csv")
         .end(function (err, res) {
           expect(res).to.have.status(200);
           expect(res.body).to.be.an("array").that.is.not.empty;
